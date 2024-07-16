2c12e041ecd62b9941af35dc266e7c37866a8f3e
jaryan728 <jaryan728@gmail.com>
2024-07-16 16:37:50 +0530

    user login db done

---
+++ Models/user.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/CodeOverflow');
+const userSchema=mongoose.Schema(
+    {
+        fname:String,
+        lname:String,
+        mob:Number,
+        git:String,
+        image:String,
+        mail:String,
+        pass:String,
+    
+        address:String
+    }
+)
+module.exports=mongoose.model('user',userSchema);
--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const cookieParser=require('cookie-parser')
 const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken')
 const path = require('path')
-
+const userModel=require('./Models/user');
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     res.render('Home');
@@ -19,4 +20,47 @@ app.get('/register',function (req,res) {
     res.render('Register');
     
 })
+app.post('/login',async (req,res)=>{
+    let {mail,pass}=req.body;
+    console.log(req.body);
+    let user=await userModel.findOne({mail});
+    if(!user)return res.send("something went wrong");
+    bcrypt.compare(req.body.pass, user.pass, function(err, result) {
+        if(result)
+            {
+                let token=jwt.sign({mail:user.mail},"secret");
+                res.cookie("token",token);
+                res.send("rvrbbb");
+            }
+            else
+            res.send("something went wrong");
+        });
+})
+app.post('/register',(req,res)=>{
+    bcrypt.genSalt(10, function(err, salt) {
+    let {fname,lname,mob,mail,address,git,pass}=req.body;
+
+       bcrypt.hash(pass, salt, async function(err, hash) {
+       let user=await userModel.create({
+        fname:fname,
+                lname:lname,
+                address:address,
+                mob:mob,
+                git:git,
+                mail:mail,
+           pass:hash
+       })
+       // to save user credential in browser
+       let token=jwt.sign({mail},"secret");
+       res.cookie("token",token);
+       res.send("Logged it");
+       });});
+  
+})
+
+app.get('/main',function (req,res) {
+    res.render('Main');
+    
+})
+
 app.listen(3000);
